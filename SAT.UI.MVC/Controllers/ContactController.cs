fe5f99f4fd9408d0b58b247ca1f119266c64a02b
emmal-original <the.original.e@gmail.com>
2020-04-10 10:24:25 -0500

    Commit Contact Form Build EL

---
+++ SAT.UI.MVC/Controllers/ContactController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SAT.UI.MVC.Models;
+using System.Net.Mail;
+using System.Net;
+
+namespace SAT.UI.MVC.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        [HttpGet]
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Contact(ContactViewModel cvm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(cvm);
+            }
+            string body = $"{cvm.Name} has sent you the following message: <br />" + $"{cvm.Message} <strong>from the email address:</strong> {cvm.Email}";
+
+            MailMessage m = new MailMessage("info@devem.com", "dplackard@outlook.com", cvm.Subject, body);
+            MailAddress copy = new MailAddress("emma_lintz@outlook.com");
+            m.CC.Add(copy);
+
+            m.IsBodyHtml = true;
+
+            m.Priority = MailPriority.High;
+
+            m.ReplyToList.Add(cvm.Email);
+
+            SmtpClient client = new SmtpClient("mail.devonplackard.com");
+
+            //client.Credentials = new NetworkCredential()
+
+            try
+            {
+                client.Send(m);
+            }
+            catch (Exception e)
+            {
+                ViewBag.ErrorMessage = e.StackTrace;
+                return View(cvm);
+            }
+
+            return View("EmailConfirmation", cvm);
+        }
+
+    }
+
+
+}
---
+++ SAT.UI.MVC/Models/ContactViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace SAT.UI.MVC.Models
+{
+    public class ContactViewModel
+    {
+        [Required(ErrorMessage ="*Name is REQUIRED")]
+        [StringLength(50, ErrorMessage="*Value must be 50 characters or less")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage ="*Email is REQUIRED")]
+        [EmailAddress]
+        [StringLength(50, ErrorMessage="*Value must be 50 characters or less")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage ="*Subject is REQUIRED")]
+        [StringLength(150, ErrorMessage="*Value must be 150 characters or less")]
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage ="*Message is required")]
+        [UIHint("MultilineText")]
+        [StringLength(1000, ErrorMessage="*Value must be 1000 characters or less")]
+        public string Message { get; set; }
+    }
+}
